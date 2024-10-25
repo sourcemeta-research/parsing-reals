13134ad0b3eb86cedbfd23ed6dd7df2e9fb8374a
Juan Cruz Viotti <jv@jviotti.com>
2024-10-25 17:15:35 -0400

    Finish C++ benchmark
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ all: .always
 	$(CMAKE) -S . -B ./build -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=OFF
 	$(CMAKE) --build ./build --config Release --parallel 4
 	$(PYTHON) benchmark_json.py 1e3.json 1>/dev/null
-	./build/benchmark_jsontoolkit 1e3.json 
+	./build/benchmark_jsontoolkit 1e3.json 1>/dev/null
 
 .always:
--- benchmark_jsontoolkit.cc
+++ benchmark_jsontoolkit.cc
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <sourcemeta/jsontoolkit/json.h>
 
-int main() {
-  std::cerr << "Hello\n";
+#include <sstream>
+#include <iostream>
+#include <chrono>
+
+int main(int, char **argv) {
+  const std::filesystem::path input_file{argv[1]};
+  auto stream{sourcemeta::jsontoolkit::read_file(input_file)};
+
+  // Parse
+  const auto timestamp_parse_start{std::chrono::high_resolution_clock::now()};
+  const auto schema{sourcemeta::jsontoolkit::parse(stream)};
+  const auto timestamp_parse_end{std::chrono::high_resolution_clock::now()};
+
+  std::ostringstream output;
+
+  // Stringify
+  const auto timestamp_stringify_start{std::chrono::high_resolution_clock::now()};
+  sourcemeta::jsontoolkit::stringify(schema, output);
+  const auto timestamp_stringify_end{std::chrono::high_resolution_clock::now()};
+
+  const auto duration_parse{std::chrono::duration_cast<std::chrono::milliseconds>(
+      timestamp_parse_end - timestamp_parse_start)};
+  const auto duration_stringify{std::chrono::duration_cast<std::chrono::milliseconds>(
+      timestamp_stringify_end - timestamp_stringify_start)};
+
+  std::cerr << "Parse time: " << duration_parse << "\n";
+  std::cerr << "Stringify time: " << duration_stringify << "\n";
+
+  std::cout << output.str() << "\n";
   return 0;
 }
